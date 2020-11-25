d58a683fd1a8dceff8d134568acc2d64ea359bc6
joshuadorsett <joshdorsett@gmail.com>
2020-11-25 12:13:31 -0500

    finished importing the UI file into project and can successfully open the app

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,10 +1,17 @@
 from PyQt5 import QtWidgets
-from PyQt5.uic import pyuic
+from PyQt5 import uic
 import sys
 
 
-class Ui(QtWidgets.QMainWindow, pyuic):
+# class for importing UI from QT Designer
+class Ui(QtWidgets.QMainWindow):
     def __init__(self):
-        super(Ui, self).__init__()  # Call the inherited classes __init__ method
-        pyuic.loadUi('RealEstatePriceEstimator.ui', self)  # Load the .ui file
+        super(Ui, self).__init__() # call inherited constructor
+        uic.loadUi('RealEstatePriceEstimator.ui', self)  # Load .ui
         self.show()  # Show the GUI
+
+
+# create app and window objects and then open GUI
+app = QtWidgets.QApplication(sys.argv)  # Create an instance of app
+window = Ui()  # create instance of Ui
+app.exec()  # execute app
